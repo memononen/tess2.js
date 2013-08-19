34c69ab8531398ec5bbe38cbd2ba2c1ecf01bf70
Mikko Mononen <memon@inside.org>
2013-08-19 21:22:39 +0300

    Added Priority Queue
    Ported priority queue, decided to skip the qsort based layer on top of
    the heap.

--- src/tess2.js
+++ src/tess2.js
@@ -959,10 +959,6 @@ Geom.intersect = function( o1, d1, o2, d2, v ) {
 
 
 
-/* Search returns the node with the smallest key greater than or equal
-* to the given key.  If there is no such key, returns a node whose
-* key is NULL.  Similarly, Succ(Max(d)) has a NULL key, etc.
-*/
 
 function DictNode() {
 	this.key = null;
@@ -992,6 +988,10 @@ Dict.prototype = {
 	},
 
 	search: function(key) {
+		/* Search returns the node with the smallest key greater than or equal
+		* to the given key.  If there is no such key, returns a node whose
+		* key is NULL.  Similarly, Succ(Max(d)) has a NULL key, etc.
+		*/
 		var node = this.head;
 		do {
 			node = node.next;
@@ -1020,6 +1020,195 @@ Dict.prototype = {
 		node.prev.next = node.next;
 	}
 };
+
+
+function PQnode() {
+	this.handle = null;
+}
+
+function PQhandleElem() {
+	this.key = null;
+	this.node = null;
+}
+
+function PriorityQ(size, leq) {
+	this.size = 0;
+	this.max = size;
+
+	this.nodes = [];
+	this.nodes.length = size+1;
+	for (var i = 0; i < this.nodes.length; i++)
+		this.nodes[i] = new PQnode();
+
+	this.handles = [];
+	this.handles.length = size+1;
+	for (var i = 0; i < this.handles.length; i++)
+		this.handles[i] = new PQhandleElem();
+
+	this.initialized = false;
+	this.freeList = 0;
+	this.leq = leq;
+
+	this.nodes[1].handle = 1;	/* so that Minimum() returns NULL */
+	this.handles[1].key = null;
+};
+
+PriorityQ.prototype = {
+
+	floatDown_: function( int curr )
+	{
+		var n = this.nodes;
+		var h = this.handles;
+		var hCurr, hChild;
+		var child;
+
+		hCurr = n[curr].handle;
+		for( ;; ) {
+			child = curr << 1;
+			if( child < this.size && this.leq( h[n[child+1].handle].key, h[n[child].handle].key )) {
+				++child;
+			}
+
+			assert(child <= this.max);
+
+			hChild = n[child].handle;
+			if( child > pq.size || this.leq( h[hCurr].key, h[hChild].key )) {
+				n[curr].handle = hCurr;
+				h[hCurr].node = curr;
+				break;
+			}
+			n[curr].handle = hChild;
+			h[hChild].node = curr;
+			curr = child;
+		}
+	},
+
+	floatUp_: function( int curr )
+	{
+		var n = this.nodes;
+		var h = this.handles;
+		var hCurr, hParent;
+		var parent;
+
+		hCurr = n[curr].handle;
+		for( ;; ) {
+			parent = curr >> 1;
+			hParent = n[parent].handle;
+			if( parent == 0 || this.leq( h[hParent].key, h[hCurr].key )) {
+				n[curr].handle = hCurr;
+				h[hCurr].node = curr;
+				break;
+			}
+			n[curr].handle = hParent;
+			h[hParent].node = curr;
+			curr = parent;
+		}
+	},
+
+	init: function() {
+		/* This method of building a heap is O(n), rather than O(n lg n). */
+		for( var i = this.size; i >= 1; --i ) {
+			this.floatDown_( i );
+		}
+		this.initialized = true;
+	},
+
+	min: function() {
+		return this.handles[this.nodes[1].handle].key;
+	},
+
+	isEmpty: function() {
+		this.size === 0;
+	},
+
+	/* really pqHeapInsert */
+	/* returns INV_HANDLE iff out of memory */
+	//PQhandle pqHeapInsert( TESSalloc* alloc, PriorityQHeap *pq, PQkey keyNew )
+	insert: function(keyNew)
+	{
+		var curr;
+		var free;
+
+		curr = ++this.size;
+		if( (curr*2) > this.max ) {
+			this.max *= 2;
+			var s;
+			s = this.nodes.length;
+			this.nodes.length = this.max+1;
+			for (var i = s; i < this.nodes.length; i++)
+				this.nodes[i] = new PQnode();
+
+			s = this.handles.length;
+			this.handles.length = this.max+1;
+			for (var i = s; i < this.handles.length; i++)
+				this.handles[i] = new PQhandleElem();
+		}
+
+		if( this.freeList === 0 ) {
+			free = curr;
+		} else {
+			free = this.freeList;
+			this.freeList = this.handles[free].node;
+		}
+
+		this.nodes[curr].handle = free;
+		this.handles[free].node = curr;
+		this.handles[free].key = keyNew;
+
+		if( this.initialized ) {
+			this.floatUp_( curr );
+		}
+		return free;
+	}
+
+	//PQkey pqHeapExtractMin( PriorityQHeap *pq )
+	extractMin: function() {
+		var n = this.nodes;
+		var h = this.handles;
+		var hMin = n[1].handle;
+		var min = h[hMin].key;
+
+		if( this.size > 0 ) {
+			n[1].handle = n[this.size].handle;
+			h[n[1].handle].node = 1;
+
+			h[hMin].key = null;
+			h[hMin].node = this.freeList;
+			this.freeList = hMin;
+
+			--this.size;
+			if( this.size > 0 ) {
+				this.floatDown_( 1 );
+			}
+		}
+		return min;
+	},
+
+	delete: function( hCurr ) {
+		var n = this.nodes;
+		var h = this.handles;
+		var curr;
+
+		assert( hCurr >= 1 && hCurr <= this.max && h[hCurr].key !== null );
+
+		curr = h[hCurr].node;
+		n[curr].handle = n[pq.size].handle;
+		h[n[curr].handle].node = curr;
+
+		--this.size;
+		if( curr <= this.size ) {
+			if( curr <= 1 || this.leq( h[n[curr>>1].handle].key, h[n[curr].handle].key )) {
+				this.floatDown_( curr );
+			} else {
+				this.floatUp_( curr );
+			}
+		}
+		h[hCurr].key = null;
+		h[hCurr].node = this.freeList;
+		this.freeList = hCurr;
+	}
+};
+
 
 
 function Tess2(opts) {
